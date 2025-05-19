07768ec4a431a4416e3d37eeee8b9dab79f57fc4
baztian <bastian.dev@gmail.com>
2025-05-19 16:51:10 +0200

    Made setup.py work

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='sqs-list-filled-queues',
     version='0.1',
-    py_modules=['sqs-list-filled-queues'],
+    py_modules=['sqs_list_filled_queues'],
     install_requires=[
         'boto3',
     ],
     entry_points={
         'console_scripts': [
-            'sqs-list-filled-queues=sqs-list-filled-queues:main',
+            'sqs-list-filled-queues = sqs_list_filled_queues:main',
         ],
     },
 )
